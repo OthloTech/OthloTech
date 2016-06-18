dca93367cb3c5688e52d9ffb97aef25bdb4c6bc7
hyde2able <hyde31415@gmail.com>
2016-06-19 04:42:14 +0900

    htmlに静的ファイルインクルードできるように

--- app/init.go
+++ app/init.go
@@ -10,14 +10,14 @@ func init() {
     router := gin.Default()
 
     // 静的ファイル配信
-    //router.Static("/css", "./dist/css")
-    //router.Static("/js", "./dist/js")
-    //router.Static("/image", "./dist/image")
-    //router.StaticFile("/favicon.ico", "./favicon.ico")
+    router.Static("/css", "../dist/css")
+    router.Static("/js", "../dist/js")
+    router.Static("/image", "../dist/image")
+    router.StaticFile("/favicon.ico", "../favicon.ico")
     router.LoadHTMLGlob("../dist/views/*.html")
 
     router.GET("/", staticHandler)
-    router.GET("/:name", handler)
+    //router.GET("/:name", handler)
 
 
     http.Handle("/", router)
