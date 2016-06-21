21ac04254aa6239d3355f4ade87a2e367c5b6bd0
pokohide <hyde31415@gmail.com>
2016-06-21 20:07:16 +0900

    nodeにサーバー移行

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const koa = require('koa')
 const _ = require('koa-route')
 const views = require('co-views')
 const app = koa()
+const serve = require('koa-static')
 
 let render = views(__dirname + '/views', { ext: 'ect' })
 let port = 8888
 let title = 'OthloTech'
+
+
+// serve files in public folder (css, js etc)
+app.use(serve(__dirname + '/dist'));
 
 app.use(_.get('/', function *() {
   this.body = yield render('main', {
@@ -22,7 +27,10 @@ app.use(_.get('/about', function *() {
 }));
   
 // dist以下のCSS, JSは静的ファイルとして提供
-app.use(require('koa-static-server')({rootDir: 'dist', rootPath: '/dist'}));
+//app.use(require('koa-static-server')({rootDir: 'css', rootPath: '/dist/css'}));
+//app.use(require('koa-static-server')({rootDir: 'js', rootPath: '/dist/js'}));
+//app.use(require('koa-static-server')({rootDir: 'image', rootPath: '/dist/image'}));
+
 
 // all other routes
 app.use(function *() {
