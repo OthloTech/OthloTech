e9fb2889428b709bd1fef5e7e4e54f8edb5ed25d
pokohide <hyde31415@gmail.com>
2016-10-22 03:45:20 +0900

    create deploy,sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# pagesフォルダの ., .., .git 以外を消去
+ls -la pages | grep -v -E '.|.git$' | xargs rm -rf
+
+# deploy用のファイルを生成
+hugo -d pages
+
+# deploy
+cd pages
+git add .
+git commit -m 'updated by deploy.sh'
+#git push origin gh-pages
