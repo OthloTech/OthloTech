8f5c817be8539b9fda212a6c240f6d3139c14f2d
pokohide <hyde31415@gmail.com>
2016-11-04 15:56:45 +0900

    cssはminしない

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,9 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const cssnext = require('postcss-cssnext')
-const cssmin = require('gulp-cssmin')
 
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
-const rename = require('gulp-rename')
 
 const paths = {
   scss: 'scss/',
@@ -21,8 +19,6 @@ gulp.task('scss', function() {
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>' )}))
     .pipe(sass())
     .pipe(postcss(processors))
-    //.pipe(cssmin())
-    //.pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.css))
 })
 
